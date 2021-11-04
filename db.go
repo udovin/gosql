365b9161f3de012f3a1379ddd9d92c788e17f602
Ivan Udovin <wilcot@ya.ru>
2021-11-04 11:12:58 +0300

    Add DB struct

--- db.go
+++ db.go
@@ -3,10 +3,11 @@ package gosql
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
-// Runner represents SQL runner interface like sql.DB or sql.Tx.
-type Runner interface {
+// WeakTx represents SQL interface like sql.DB or sql.Tx.
+type WeakTx interface {
 	// Exec executes a query that doesn't return rows.
 	Exec(query string, args ...interface{}) (sql.Result, error)
 	// Query executes a query that returns rows, typically a SELECT.
@@ -22,3 +23,50 @@ type TxBeginner interface {
 	// BeginTx starts a transaction.
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 }
+
+func WithTx(b TxBeginner, fn func(tx *sql.Tx) error) error {
+	tx, err := b.BeginTx(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			// Try to rollback transaction on panic.
+			_ = tx.Rollback()
+			panic(r)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		return tx.Rollback()
+	} else {
+		return tx.Commit()
+	}
+}
+
+func WithEnsuredTx(tx WeakTx, fn func(tx *sql.Tx) error) error {
+	switch v := tx.(type) {
+	case *sql.Tx:
+		return fn(v)
+	case TxBeginner:
+		return WithTx(v, fn)
+	default:
+		panic(fmt.Errorf("unsupported type: %T", v))
+	}
+}
+
+// DB represents wrapper for sql.DB with additional builder and
+// read-only connection.
+type DB struct {
+	// Read-write connection.
+	*sql.DB
+	// Read-only connection.
+	RO *sql.DB
+	// Builder contains builder for specified database dialect.
+	Builder
+}
+
+// Test *DB for interfaces.
+var (
+	_ WeakTx     = &DB{}
+	_ TxBeginner = &DB{}
+)
